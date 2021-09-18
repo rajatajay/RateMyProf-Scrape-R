b1a35d8baefb373d6d82ecb50f773ca0388cb033
rajatajay <57448871+rajatajay@users.noreply.github.com>
2021-09-18 19:16:30 -0400

    Code to scrape all data

---
+++ Babson.R
@@ -0,0 +1,117 @@
+library(RSelenium)
+library(rJava)      # Needed for tabulizer
+library(tabulizer)  # Handy tool for PDF Scraping
+library(tidyverse)  # Core data manipulation and visualization libraries
+library(rvest)
+library(xlsx)
+
+eCaps <- list(chromeOptions = list(
+  args = c('--headless', '--disable-gpu', '--window-size=1280,800')
+))
+
+rd <- rsDriver(extraCapabilities = eCaps)
+ffd <- rd$client
+ffd$navigate("https://www.ratemyprofessors.com/search/teachers?query=*&sid=73") #Currently entered Babson College, US
+ffd$screenshot(display = TRUE)
+
+Sys.sleep(2)
+
+load_btn <- ffd$findElement(using = "css", value = ".ecgEHi") #To get rid of the pop-up
+load_btn$clickElement()
+
+check <- 1
+
+while (check == 1)
+{
+  load_btn <- ffd$findElement(using = "css", value = ".eaZArN")
+  #page = read_html(ffd$getPageSource()[[1]])
+  load_btn$clickElement()
+  Sys.sleep(10)
+}
+
+page = read_html(ffd$getPageSource()[[1]])
+pagecopy <- page
+
+name = page %>% html_nodes(".cJdVEK") %>% html_text()
+quality = page %>% html_nodes(".CardNumRating__CardNumRatingNumber-sc-17t4b9u-2") %>% html_text()
+difficulty = page %>% html_nodes(".enhFnm+ .fyKbws .hroXqf") %>% html_text()
+noRate = page %>% html_nodes(".jMRwbg") %>% html_text()
+wouldtakeagain = page %>% html_nodes(".fyKbws:nth-child(1) .hroXqf") %>% html_text()
+course = page %>% html_nodes(".haUIRO") %>% html_text()
+campus = page %>% html_nodes(".iDlVGM") %>% html_text()
+
+##################################################################################
+namelink <- page %>% html_nodes(".dLJIlx") %>% html_attr("href") %>% paste("https://www.ratemyprofessors.com", ., sep = "") 
+##################################################################################
+
+profess = data.frame(name, quality, difficulty, noRate, wouldtakeagain, course, campus,namelink, stringsAsFactors =  FALSE)
+
+write.csv(profess, "BabsonCollege.csv") #change uni name each time
+
+
+Sys.sleep(20)
+
+profile <- data.frame(namelink, name)
+
+for (i in 1:nrow(profile)) {
+
+  ffd$navigate(paste('',profile[i,1],'',sep = ""))
+
+  suppressMessages(try(load_btn <- ffd$findElement(using = "css", value = ".ecgEHi"), silent = TRUE)) #To get rid of the pop-up
+  suppressMessages(try(load_btn$clickElement(), silent = TRUE))
+
+  NumRate <- 0
+  NumRate <- ((strtoi(strsplit(profess[i,4], " ")[[1]][1])) %/% 10)
+  NumRate <- NumRate+10 #number of iterations
+
+  for (a in 1:NumRate)
+  {
+    suppressMessages(try(load_btn <- ffd$findElement(using = "css", value = ".eaZArN"), silent = TRUE))
+    suppressMessages(try(load_btn$clickElement(), silent = TRUE))
+    print("running")
+  }
+
+  print(NumRate)
+  print("outside loop")
+  page = read_html(ffd$getPageSource()[[1]])
+
+  quality_indv = page %>% html_nodes(".DObVa:nth-child(1) .CardNumRating__CardNumRatingNumber-sc-17t4b9u-2") %>% html_text()
+  difficulty_indv = page %>% html_nodes(".cDKJcc") %>% html_text()
+  description_indv = page %>% html_nodes(".gRjWel") %>% html_text()
+  course_indv = page %>% html_nodes(".gxDIt") %>% html_text()
+  date_indv = page %>% html_nodes(".BlaCV") %>% html_text()
+
+  quality_indv <- data.frame(quality_indv)
+  difficulty_indv <- data.frame(difficulty_indv)
+  description_indv <- data.frame(description_indv)
+  course_indv <- data.frame(course_indv)
+  date_indv <- data.frame(date_indv)
+  courseDate <- data.frame(course_indv, date_indv)
+
+  x = 1
+  while (x < nrow(courseDate)) {
+    if ((courseDate[x,1] == courseDate[x+1,1])){
+      courseDate[x,1] = "remove"
+      #print(courseDate[x+1,])
+      x <-  x+2
+    }
+    else {
+      x <-  x+1
+    }
+  }
+  freshcoursedate <- courseDate[courseDate$course != "remove",]
+
+  professInfo = data.frame(freshcoursedate, quality_indv, difficulty_indv, stringsAsFactors =  FALSE, check.rows = TRUE)
+
+  ProfnameSearch <- paste(profess[i,1], "at", profess[i,7],"RateMyProfessor", sep = " ")
+  CSVname <- paste(ProfnameSearch,".csv",sep = "")
+
+  DescriptionName <- paste("Description ", ProfnameSearch,".csv",sep = "")
+
+  write.csv(description_indv, paste('C:\\Users\\Rajat\\Desktop\\Runny\\R\\Babson\\Descriptions\\', DescriptionName, sep = ""), row.names = FALSE)
+
+  write.csv(professInfo, paste('C:\\Users\\Rajat\\Desktop\\Runny\\R\\Babson\\',CSVname, sep = ""), row.names = FALSE)
+
+
+}
+
---
+++ HeadlessChrome.R
@@ -0,0 +1,91 @@
+library(RSelenium)
+library(rvest)
+library(stringr)
+eCaps <- list(chromeOptions = list(
+  args = c('--headless', '--disable-gpu', '--window-size=1280,800')
+))
+rD <- rsDriver(extraCapabilities = eCaps,
+               chromever = "latest_compatible")
+ffd <- rD$client
+
+namelink <- read.csv("namelinkyorku2.csv")
+profile <- data.frame(namelink[,2])
+
+for (i in 3570:nrow(namelink)) { #plus 40 of YorkUKeele File Run
+  
+  print(i)
+  ffd$navigate(paste('',namelink[i,2],'',sep = ""))
+  
+  suppressMessages(try(load_btn <- ffd$findElement(using = "css", value = ".ecgEHi"), silent = TRUE)) #To get rid of the pop-up
+  suppressMessages(try(load_btn$clickElement(), silent = TRUE))
+  
+  page <- read_html(ffd$getPageSource()[[1]])
+  ratingTitle <- page %>% html_nodes(".jMkisx a") %>% html_text()
+  nameTitle <- page %>% html_nodes(".cfjPUG") %>% html_text()
+  UniTitle <- page %>% html_nodes(".iLYGwn a") %>% html_text()
+  
+  if (ratingTitle == "Add a rating."){
+    next
+  }
+  else{
+  
+  NumRate <- 0
+  NumRate <- (as.numeric(str_extract_all(ratingTitle, "[0-9]+")[[1]]) %/% 10) 
+  
+  NumRate <- NumRate+3 #number of iterations
+  NumRate
+  for (a in 1:NumRate)
+  {
+    suppressMessages(try(load_btn <- ffd$findElement(using = "css", value = ".eaZArN"), silent = TRUE))
+    suppressMessages(try(load_btn$clickElement(), silent = TRUE))
+    
+    if (NumRate > 25){Sys.sleep(8)}
+    print(a)
+  }
+  
+  print(nameTitle)
+  print(NumRate)
+  print("outside loop")
+  page = read_html(ffd$getPageSource()[[1]])
+  
+  
+  quality_indv = page %>% html_nodes(".DObVa:nth-child(1) .CardNumRating__CardNumRatingNumber-sc-17t4b9u-2") %>% html_text()
+  difficulty_indv = page %>% html_nodes(".cDKJcc") %>% html_text()
+  description_indv = page %>% html_nodes(".gRjWel") %>% html_text()
+  course_indv = page %>% html_nodes(".gxDIt") %>% html_text()
+  date_indv = page %>% html_nodes(".BlaCV") %>% html_text()
+  
+  quality_indv <- data.frame(quality_indv)
+  difficulty_indv <- data.frame(difficulty_indv)
+  description_indv <- data.frame(description_indv)
+  course_indv <- data.frame(course_indv)
+  date_indv <- data.frame(date_indv)
+  courseDate <- data.frame(course_indv, date_indv)
+  
+  x = 1
+  while (x < nrow(courseDate)) {
+    if ((courseDate[x,1] == courseDate[x+1,1])){
+      courseDate[x,1] = "remove"
+      #print(courseDate[x+1,])
+      x <-  x+2
+    }
+    else {
+      x <-  x+1
+    }
+  }
+  freshcoursedate <- courseDate[courseDate$course != "remove",]
+  
+  professInfo = data.frame(freshcoursedate, quality_indv, difficulty_indv, stringsAsFactors =  FALSE, check.rows = TRUE)
+  
+  ProfnameSearch <- paste(strsplit(nameTitle, split = " ")[[1]][1], strsplit(nameTitle, split = " ")[[1]][2], "at", UniTitle,"RateMyProfessor", sep = " ")
+  CSVname <- paste(ProfnameSearch,".csv",sep = "")
+  
+  DescriptionName <- paste("Description ", ProfnameSearch,".csv",sep = "")
+  
+  write.csv(description_indv, paste('C:\\Users\\Rajat\\Desktop\\Runny\\R\\YorkUKeele\\Descriptions\\', DescriptionName, sep = ""), row.names = FALSE)
+  
+  write.csv(professInfo, paste('C:\\Users\\Rajat\\Desktop\\Runny\\R\\YorkUKeele\\',CSVname, sep = ""), row.names = FALSE)
+  
+  } 
+}
+
